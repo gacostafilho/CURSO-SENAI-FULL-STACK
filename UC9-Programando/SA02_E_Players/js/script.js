5710b6dc3775941d34149e912ce96627e651526f
gacostafilho <gacostafilho@gmail.com>
2023-02-11 15:26:10 -0300

    UC-9 SA2 Atividade Online 3

--- UC9-Programando/SA02_E_Players/js/script.js
+++ UC9-Programando/SA02_E_Players/js/script.js
@@ -2,3 +2,16 @@
       const myElement = document.querySelector('.ctnWelcome')
       myElement.style.display = "none";
   }
+
+  function validateForm() {
+    let xEmail = document.querySelector('#email-login').value;
+    let xPassword = document.querySelector('#password-login').value;
+    if (xEmail == "") {
+      alert("O campo email deve ser preenchido");
+      return false
+    };
+    if (xPassword == "") {
+      alert("O campo senha deve ser preenchido");
+      return false
+    }
+  }
